8b0eb3d4e0c589a9c08a444c6388a7562016c05b
Vishad Tyagi <vishad@aresourcepool.com>
2025-02-01 11:34:11 +0530

    fix current day

--- src/components/DayGrid.js
+++ src/components/DayGrid.js
@@ -1,5 +1,5 @@
 const DayGrid = ({ year, month, date }) => {
-  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat",];
+  const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
   let rows = [];
   for (let i = 0; i < days.length; i++) {
@@ -7,7 +7,7 @@ const DayGrid = ({ year, month, date }) => {
     let k = 0;
     for (let j = 0; j < days.length; j++) {
       const dayIndex = i + j < days.length ? i + j : k++;
-      const className = (dayIndex === new Date(year, month, date).getDay()) ? "day-columns columns current-day " : "day-columns columns ";
+      const className = (dayIndex === (new Date(year, month, date).getDay() - 1)) ? "day-columns columns current-day " : "day-columns columns ";
       columns.push(
         <td
           key={dayIndex}
